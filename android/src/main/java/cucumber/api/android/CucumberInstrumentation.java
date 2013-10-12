7022f0b998e2f1bc22f0a569f03a91737537de62
Frieder Bluemle <info@bluemle.net>
2013-10-11 19:17:46 -0700

    Add support for 'coverage' option
    This adds support to generate EMMA code coverage reports when using
    CucumberInstrumentation. It is pretty much a 1:1 copy from
    InstrumentationTestRunner.

--- android/src/main/java/cucumber/api/android/CucumberInstrumentation.java
+++ android/src/main/java/cucumber/api/android/CucumberInstrumentation.java
@@ -28,13 +28,18 @@ import dalvik.system.DexFile;
 import gherkin.formatter.Formatter;
 import gherkin.formatter.Reporter;
 
+import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CucumberInstrumentation extends Instrumentation {
     public static final String REPORT_VALUE_ID = "CucumberInstrumentation";
     public static final String REPORT_KEY_NUM_TOTAL = "numtests";
+    private static final String REPORT_KEY_COVERAGE_PATH = "coverageFilePath";
+    private static final String DEFAULT_COVERAGE_FILE_NAME = "coverage.ec";
     public static final int DEFAULT_DEBUGGER_TIMEOUT = 10000;
     public static final String TAG = "cucumber-android";
 
@@ -42,6 +47,8 @@ public class CucumberInstrumentation extends Instrumentation {
     private int debuggerTimeout;
     private boolean justCount;
     private int testCount;
+    private boolean coverage;
+    private String coverageFilePath;
 
     private RuntimeOptions runtimeOptions;
     private ResourceLoader resourceLoader;
@@ -65,6 +72,8 @@ public class CucumberInstrumentation extends Instrumentation {
                 }
             }
             justCount = getBooleanArgument(arguments, "count");
+            coverage = getBooleanArgument(arguments, "coverage");
+            coverageFilePath = arguments.getString("coverageFile");
         }
         Context context = getContext();
         classLoader = context.getClassLoader();
@@ -136,6 +145,10 @@ public class CucumberInstrumentation extends Instrumentation {
 
             printSummary();
 
+            if (coverage) {
+                generateCoverageReport();
+            }
+
             finish(Activity.RESULT_OK, results);
         }
     }
@@ -184,5 +197,59 @@ public class CucumberInstrumentation extends Instrumentation {
         for (String s : runtime.getSnippets()) {
             Log.w(TAG, s);
         }
+    }
+
+    private void generateCoverageReport() {
+        // use reflection to call emma dump coverage method, to avoid
+        // always statically compiling against emma jar
+        String coverageFilePath = getCoverageFilePath();
+        java.io.File coverageFile = new java.io.File(coverageFilePath);
+        try {
+            Class<?> emmaRTClass = Class.forName("com.vladium.emma.rt.RT");
+            Method dumpCoverageMethod = emmaRTClass.getMethod("dumpCoverageData",
+                    coverageFile.getClass(), boolean.class, boolean.class);
+
+            dumpCoverageMethod.invoke(null, coverageFile, false, false);
+            // output path to generated coverage file so it can be parsed by a test harness if
+            // needed
+            results.putString(REPORT_KEY_COVERAGE_PATH, coverageFilePath);
+            // also output a more user friendly msg
+            final String currentStream = results.getString(
+                    Instrumentation.REPORT_KEY_STREAMRESULT);
+            results.putString(Instrumentation.REPORT_KEY_STREAMRESULT,
+                String.format("%s\nGenerated code coverage data to %s", currentStream,
+                coverageFilePath));
+        } catch (ClassNotFoundException e) {
+            reportEmmaError("Is emma jar on classpath?", e);
+        } catch (SecurityException e) {
+            reportEmmaError(e);
+        } catch (NoSuchMethodException e) {
+            reportEmmaError(e);
+        } catch (IllegalArgumentException e) {
+            reportEmmaError(e);
+        } catch (IllegalAccessException e) {
+            reportEmmaError(e);
+        } catch (InvocationTargetException e) {
+            reportEmmaError(e);
+        }
+    }
+
+    private String getCoverageFilePath() {
+        if (coverageFilePath == null) {
+            return getTargetContext().getFilesDir().getAbsolutePath() + File.separator +
+                   DEFAULT_COVERAGE_FILE_NAME;
+        } else {
+            return coverageFilePath;
+        }
+    }
+
+    private void reportEmmaError(Exception e) {
+        reportEmmaError("", e);
+    }
+
+    private void reportEmmaError(String hint, Exception e) {
+        String msg = "Failed to generate emma coverage. " + hint;
+        Log.e(TAG, msg, e);
+        results.putString(Instrumentation.REPORT_KEY_STREAMRESULT, "\nError: " + msg);
     }
 }
